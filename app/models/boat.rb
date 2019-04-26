b5b6660ee666829c0628cd8f45f8732b3a9d250e
BebopVinh <vinhdo07@gmail.com>
2019-04-26 10:18:56 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,18 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  private
+
+    def self.first_five
+      Boat.all[0..4]
+    end
+
+    def self.dinghy
+      Boat.select{|boat| boat if boat.length < 20}
+    end
+
+    def self.ship
+      Boat.select{|boat| boat if boat.length >= 20}
+    end
 end

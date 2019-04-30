331d12bfd7f25fc34a593e43a7baf050c661c0f9
github username <github email address>
2019-04-30 08:23:42 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,39 +4,40 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    @boats = Boat.all[0..4]
+    Boat.all[0..4]
   end
 
   def self.dinghy
-    @boats = Boat.where("length < ?", 20)
+    Boat.where("length < ?", 20)
   end
 
   def self.ship
-    @boats = Boat.where("length >= ?", 20)
+    Boat.where("length >= ?", 20)
   end
 
   def self.last_three_alphabetically
-    @boats = (Boat.all.sort_by{|b| b.name.downcase}).last(3).reverse
+    Boat.order(name: :desc).first(3)
+    #Boat.all.sort_by{|b| b.name.downcase}.last(3).reverse
   end
 
   def self.without_a_captain
-    @boats = Boat.where("captain_id is NULL")
+    Boat.where("captain_id is NULL")
   end
 
   def self.sailboats
-    @boats = Boat.all.select {|boat| boat.classifications.map(&:name).include?("Sailboat")}
+    Boat.all.select {|boat| boat.classifications.map(&:name).include?("Sailboat")}
   end
 
   def self.catamarans
-    @boats = Boat.all.select {|boat| boat.classifications.map(&:name).include?("Catamaran")}
+    Boat.all.select {|boat| boat.classifications.map(&:name).include?("Catamaran")}
   end
 
   def self.motorboats
-    @boats = Boat.all.select {|boat| boat.classifications.map(&:name).include?("Motorboat")}
+    Boat.all.select {|boat| boat.classifications.map(&:name).include?("Motorboat")}
   end
 
   def self.with_three_classifications
-    @boats = Boat.all.select {|boat| boat.classifications.count == 3}
+    Boat.all.select {|boat| boat.classifications.count == 3}
   end
 
   def self.longest

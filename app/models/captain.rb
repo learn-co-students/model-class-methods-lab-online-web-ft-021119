2a0f9a3761e664f325f433ee6ecaf40caab3ab99
ChrstphGrnr <ChristophGruener@me.com>
2019-05-07 16:05:50 -0600

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seafarers
+    sail_caps = Boat.sailboats.map(&:captain).uniq
+    motor_caps = Boat.motorboats.map(&:captain).uniq
+    seafarers = sail_caps & motor_caps
+  end
+
+  def self.non_sailors
+    self.all - self.sailors
+  end
+  
 end

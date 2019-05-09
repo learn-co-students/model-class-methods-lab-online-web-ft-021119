9d01b57cfc1f48920ad90d2d6ba84eb774e1e876
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-08 20:26:38 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,47 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    first(5)
+  end
+
+  def self.dinghy
+    where(length: (0..20))
+  end
+
+  def self.ship
+    where.not(length: (0..20))
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).first(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    sailboats = []
+    classification = Classification.where(name: "Sailboat")
+    BoatClassification.where(classification_id: classification.first.id).each do |x|
+      sailboats << Boat.where(id: x.boat_id).first
+    end
+    sailboats
+  end
+
+  def self.with_three_classifications
+    multi_class = []
+    three_class = []
+    all.each do |boat|
+      boat_class = BoatClassification.where(boat_id: boat.id)
+      multi_class << boat_class if boat_class.length >= 3
+    end
+    multi_class.each do |boat|
+      boat.delete unless boat.length == 3
+      three_class << Boat.where(id: boat.first.boat_id).first
+    end
+    three_class
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    classifications = []
+    longest_boat = Boat.order(length: :desc).limit(1).first.id
+    join = BoatClassification.where(boat_id: longest_boat)
+    join.each do |x|
+      classifications << Classification.where(id: x.classification_id).first
+    end
+    classifications
+  end
 end

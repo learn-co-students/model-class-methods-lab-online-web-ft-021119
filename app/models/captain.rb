70733b13ed53e560f2b6b4aa0d72507111445189
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-12 00:07:27 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #returns all captains of catamarans
+    #Captain.all.select {|captain| captain.boats.classifications.map(&:name).include?("Catamaran")}
+    classif = Classification.find_by(name: "Catamaran")
+    classif.boats.map { |boat| boat.captain} 
+  end
+
+  def self.sailors
+    #returns captains with sailboats
+    Boat.sailboats.map(&:captain).compact.uniq
+  end
+
+  def self.talented_seafarers
+    sailboat = Boat.sailboats.map(&:captain).compact.uniq
+    motorboat = Boat.motorboats.map(&:captain).compact.uniq
+    captains = sailboat & motorboat
+  end
+
+  def self.non_sailors
+    #returns people who are not captains of sailboats
+    Captain.all - Captain.sailors
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,16 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    #returns all classifications
+    Classification.all
+  end
+
+  def self.longest
+    #returns the classifications for the longest boat
+
+    Boat.longest.classifications
+  end
+
 end
